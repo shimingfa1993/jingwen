6f6227d03f7d3c2b6d0a6d11c4bb744c916dfe80
shimingfa <yxkj.shimingfa@aisino.com>
2025-06-25 16:16:48 +0800

    \修复图片和视频

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' 
-    ? '/jingwen/' 
-    : '/',
+  publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false,
